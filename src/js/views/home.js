ebdb72f7f4e475e198cfdd433a91f0227b0e6171
oriaror <farraxovfarraxov@gmail.com>
2024-05-19 21:10:54 +0400

    commit10

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,7 +3,7 @@ export default () => /*html*/ `
 
     <div class='posts'>
           <div class='post'>
-               <img class='postImg' src='public/784a0ab95cd5be67b447f5bfbc65d94f.jpg' alt='ss' />
+               <img class='postImg' src=${"./public/land.jpg"} alt='ss' />
                <div class='postComment'>
                <img src='./public/icons/Union.png' alt='ss'/>
                   <p style='display: inline; padding: 5px; color: #5095EC'>Jason Anderson</p>commented:
